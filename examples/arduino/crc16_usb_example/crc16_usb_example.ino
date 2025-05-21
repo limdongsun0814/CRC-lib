a76e73bbeade9b989c75986be60c9f818eb3a25a
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2025-05-21 16:24:03 +0900

    arduino example 추가

--- examples/arduino/crc16_usb_example/crc16_usb_example.ino
+++ examples/arduino/crc16_usb_example/crc16_usb_example.ino
@@ -1,11 +1,11 @@
-#include <crc16_modbus.h>
+#include <crc16_usb.h>
 
 void setup() {
     Serial.begin(9600); 
     uint8_t data[] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x02}; 
     uint8_t length = sizeof(data);
     uint16_t crc = calculate(data, length);
-    Serial.print("CRC-16 Modbus: 0x");
+    Serial.print("CRC16-USB: 0x");
     Serial.println(crc, HEX); 
 }
 
